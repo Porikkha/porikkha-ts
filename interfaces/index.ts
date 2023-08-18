ef7d46382911806853c3cf7489c26d5876cfb918
smilodon <rakinkzs@gmail.com>
2023-08-18 22:27:49 +0600

    merged with master

---
+++ interfaces/index.ts
@@ -0,0 +1,7 @@
+export type {default as Question} from './question/Question'
+export type {default as SingleChoiceQuestion} from './question/SingleChoiceQuestion'
+export type {default as MultipleChoiceQuestion} from './question/MultipleChoiceQuestion'
+export type {default as ShortAnswerQuestion} from './question/ShortAnswerQuestion'
+export type {default as User} from './User'
+export type {default as Exam} from "./Exam"
+export * from './question/Question'
