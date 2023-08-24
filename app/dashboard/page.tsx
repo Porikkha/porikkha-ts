a5c153e50fa6d5af240c288f99e5eab762e4c45b
Sohaib03 <sohaibbinmusa@gmail.com>
2023-08-24 21:18:24 +0600

    Changed Sidebar icons

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,118 +1,146 @@
-"use client";
+'use client';
 
-import { Button, Chip, CircularProgress, Divider, Typography } from "@mui/joy";
-import { useEffect, useState } from "react";
-import Exam, { dummyExam } from "@/interfaces/Exam";
-import { useSession } from "next-auth/react";
-import formatTime, { formatDuration } from "@/utils/timeUtils";
+import { Button, Chip, CircularProgress, Divider, Typography } from '@mui/joy';
+import { useEffect, useState } from 'react';
+import Exam, { dummyExam } from '@/interfaces/Exam';
+import { useSession } from 'next-auth/react';
+import formatTime, { formatDuration } from '@/utils/timeUtils';
 import { useRouter, useSearchParams } from 'next/navigation';
-import SuccessAlert from "@/components/ui/SuccessAlert";
-import { Router } from "next/router";
-import Sidebar from "@/components/dashboard/Sidebar";
+import SuccessAlert from '@/components/ui/SuccessAlert';
+import { Router } from 'next/router';
+import Sidebar from '@/components/dashboard/Sidebar';
 
 export default function Page() {
-    const [username, setUsername] = useState("Alex");
-    const [exams, setExams] = useState([dummyExam]);
-    const [loading, setLoading] = useState(true);
-    const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const [username, setUsername] = useState('Alex');
+  const [exams, setExams] = useState([dummyExam]);
+  const [loading, setLoading] = useState(true);
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
 
-    const router = useRouter();
-    const searchParams = useSearchParams()
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-    const editExam = (examId : any) => {
-        router.push(`/exam/create/${examId}`);
-    }
+  const editExam = (examId: any) => {
+    router.push(`/exam/create/${examId}`);
+  };
 
-    if (searchParams.has("status") && searchParams.get("status") == "success") {
-        console.log("Slow Alert") ;
-    }
+  if (searchParams.has('status') && searchParams.get('status') == 'success') {
+    console.log('Slow Alert');
+  }
 
-    const { data: session } = useSession();
-    console.log('😁 User: ', session?.user);
+  const { data: session } = useSession();
+  console.log('😁 User: ', session?.user);
 
-    const fetchExams = async () => {
-        if (!session?.user?.id) return;
-        const res = await fetch(`/api/exams/getAll/${session?.user?.id}`);
-        const data = await res.json();
-        console.log(data);
-        let newExamList = data.exams.map((exam: any) => {
-            let e : Exam = {
-                examId: exam.id, 
-                creatorId: exam.creatorId,
-                description: exam.description,
-                duration: exam.duration,
-                startTime: new Date(exam.startTime),
-                title: exam.title,
-                questions: [],
-                allowedAbilities: []
-            }
-            return e;
-        });
-        console.log("🚀 ~ file: page.tsx:37 ~ newExamList ~ newExamList:", newExamList)
+  const fetchExams = async () => {
+    if (!session?.user?.id) return;
+    const res = await fetch(`/api/exams/getAll/${session?.user?.id}`);
+    const data = await res.json();
+    console.log(data);
+    let newExamList = data.exams.map((exam: any) => {
+      let e: Exam = {
+        examId: exam.id,
+        creatorId: exam.creatorId,
+        description: exam.description,
+        duration: exam.duration,
+        startTime: new Date(exam.startTime),
+        title: exam.title,
+        questions: [],
+        allowedAbilities: [],
+      };
+      return e;
+    });
+    console.log('🚀 ~ file: page.tsx:37 ~ newExamList ~ newExamList:', newExamList);
 
-        setExams(newExamList);
-        // setExams(data);
-        setLoading(false);
-    }
+    setExams(newExamList);
+    // setExams(data);
+    setLoading(false);
+  };
 
-    useEffect(() => {
-        console.log("Call useeffect")
-        fetchExams();
-    }, [session?.user?.id]);
+  useEffect(() => {
+    console.log('Call useeffect');
+    fetchExams();
+  }, [session?.user?.id]);
 
-    const ExamGrid = () => {
-        return (
-            <div className="p-5">
-                <div className="flex flex-wrap -mx-4">
-                    {exams.map((exam, index) => (
-                        <div className="w-1/3 px-4 mb-4" key={index}>
-                            <ExamCard exam={exam} editExam={editExam}/>
-                        </div>
-                    ))}
+  const ExamGrid = () => {
+    return (
+      <div className='p-5'>
+        <div className='-mx-4 flex flex-wrap'>
+          {exams.map((exam, index) => (
+            <div className='mb-4 w-1/3 px-4' key={index}>
+              <ExamCard exam={exam} editExam={editExam} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <Sidebar />
+      <section className='w-full'>
+        <div className='grid grid-cols-4 grid-flow-col'> 
+            <div className='col-span-3'>
+                <div className='mx-auto rounded-md h-screen bg-slate-50'>
+                <Typography className='pb-2 text-3xl'>Welcome {username}</Typography>
+                <Divider className='bg-slate-200' />
+                <Typography className='pt-2 text-sm'>
+                    Your have scheduled{' '}
+                    <span className='text-purple-400'> {exams.length} Exams </span>{' '}
+                </Typography>
                 </div>
             </div>
-        );
-    }
-
-
-	return (
-        <>
-        <Sidebar />
-		<section className="w-full">
-			<div className="w-4/5 mx-auto bg-white rounded-md p-5">
-                <Typography className="text-3xl pb-2">Welcome {username}</Typography>
-                <Divider className="bg-slate-200"/>
-                <Typography className="text-sm pt-2">Your have scheduled <span className="text-purple-400"> {exams.length} Exams </span> </Typography>
-
-
-                { loading && <div className="w-full flex justify-center py-10"> <CircularProgress variant="soft"/> </div>}
-                { !loading && <ExamGrid /> }
-                <SuccessAlert
-                    showSuccessAlert={showSuccessAlert}
-                    setShowSuccessAlert={setShowSuccessAlert}
-                />
-			</div>
-		</section>
-        </>
-	);
-}
-
-const ExamCard = ({exam, editExam} : any) => {
-    return (
-        <div className="w-64 bg-fade-purple hover:border-cyan-100 rounded-md p-5">
-            <Typography className="text-sm pb-1 font-bold">{exam.title}</Typography>
-            <Divider className="bg-slate-200"/>
-            <Typography className="text-xs pt-1 font-medium">Start : { formatTime(exam.startTime)} </Typography>
-            <Typography className="text-xs pt-1 font-medium">Duration : { formatDuration(exam.duration) } </Typography>
-            <Typography className="text-xs pt-1 font-medium">Submissions : {0} </Typography>
-
-            <div className="w-full flex justify-center">
-            <Button className="bg-white text-purple-500 border-purple-500 hover:text-white hover:bg-purple-300 border-2 rounded-md mt-5" 
-                onClick={
-                    () => editExam(exam.examId)
-                }
-            >View</Button>
+            <div className='col-span-1'>
+                <div className='mx-auto rounded-md bg-slate-100 p-5'>
+                    <Typography className='pt-2 text-sm'> Calendar </Typography>
+                </div>
             </div>
         </div>
-    )
+        <div className='mx-auto w-4/5 rounded-md bg-white p-5'>
+          <Typography className='pb-2 text-3xl'>Welcome {username}</Typography>
+          <Divider className='bg-slate-200' />
+          <Typography className='pt-2 text-sm'>
+            Your have scheduled{' '}
+            <span className='text-purple-400'> {exams.length} Exams </span>{' '}
+          </Typography>
+
+          {loading && (
+            <div className='flex w-full justify-center py-10'>
+              {' '}
+              <CircularProgress variant='soft' />{' '}
+            </div>
+          )}
+          {!loading && <ExamGrid />}
+          <SuccessAlert
+            showSuccessAlert={showSuccessAlert}
+            setShowSuccessAlert={setShowSuccessAlert}
+          />
+        </div>
+      </section>
+    </>
+  );
 }
+
+const ExamCard = ({ exam, editExam }: any) => {
+  return (
+    <div className='w-64 rounded-md bg-fade-purple p-5 hover:border-cyan-100'>
+      <Typography className='pb-1 text-sm font-bold'>{exam.title}</Typography>
+      <Divider className='bg-slate-200' />
+      <Typography className='pt-1 text-xs font-medium'>
+        Start : {formatTime(exam.startTime)}{' '}
+      </Typography>
+      <Typography className='pt-1 text-xs font-medium'>
+        Duration : {formatDuration(exam.duration)}{' '}
+      </Typography>
+      <Typography className='pt-1 text-xs font-medium'>Submissions : {0} </Typography>
+
+      <div className='flex w-full justify-center'>
+        <Button
+          className='mt-5 rounded-md border-2 border-purple-500 bg-white text-purple-500 hover:bg-purple-300 hover:text-white'
+          onClick={() => editExam(exam.examId)}
+        >
+          View
+        </Button>
+      </div>
+    </div>
+  );
+};
--- components/dashboard/Sidebar.tsx
+++ components/dashboard/Sidebar.tsx
@@ -3,15 +3,16 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import HomeIcon from '@mui/icons-material/Home';
 import AppsIcon from '@mui/icons-material/Apps';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
-
+import { LuBarChart, LuBarChart4, LuHome, LuLogOut, LuSettings, LuUser } from "react-icons/lu";
 export default function Sidebar() {
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 m-0
-                    flex flex-col justify-center bg-gray-900 text-white shadow-lg">
-            <SideBarIcon icon={<HomeIcon />}/>
-            <SideBarIcon icon={<AppsIcon />}/>
-            <SideBarIcon icon={<NotificationsNoneOutlinedIcon />}/>
-            <SideBarIcon icon={<LogoutIcon />}/>
+        <div className="fixed top-0 left-0 border h-screen w-16 m-0
+                    flex flex-col justify-center bg-slate-50 -purple text-white shadow-lg">
+            <SideBarIcon icon={<LuHome />}/>
+            <SideBarIcon icon={<LuUser />}/>
+            <SideBarIcon icon={<LuSettings />}/>
+            <SideBarIcon icon={<LuBarChart4 />}/>
+            <SideBarIcon icon={<LuLogOut />}/>
         </div>
     )
 }
