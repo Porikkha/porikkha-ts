30d652e6368013538a0ddfcc8b4cea418c9a4ad5
smilodon <rakinkzs@gmail.com>
2023-08-25 20:52:18 +0600

    deployment: next_auth secret error trial fix

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
-import { withAuth } from "next-auth/middleware";
+// import { withAuth } from "next-auth/middleware";
 
-// export {default} from "next-auth/middleware"
+export {default} from "next-auth/middleware"
 // export { default } from "next-auth/middleware" 
 
-export default withAuth({
-    secret: process.env.NEXTAUTH_SECRET as string
-}) ;
+// export default withAuth({
+    // secret: process.env.NEXTAUTH_SECRET as string
+// }) ;
 export const config = {
     matcher: ["/dashboard/:path*", "/profile/:path*", "/exams/:path*"]
 }
