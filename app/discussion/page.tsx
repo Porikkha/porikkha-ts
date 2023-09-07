e1c9d1e2e056d91fb54abb5138a82cd2682de858
Sohaib03 <sohaibbinmusa@gmail.com>
2023-09-07 21:39:53 +0600

    Refactoring

--- app/discussion/page.tsx
+++ app/discussion/page.tsx
@@ -14,18 +14,8 @@ export default function Page() {
       <Sidebar />
       <section className='w-full pl-16'>
         <div className='grid grid-cols-7 gap-2 p-5'>
-          <div className='col-span-7 h-screen rounded-md bg-light-gray p-5'>
+          <div className='col-span-7 min-h-screen rounded-md bg-light-gray p-5'>
             <div className='flex' style={{justifyContent:"space-between"}}>
-              {/* <Typography className='pb-2 text-3xl font-semibold'>
-                Operating Systems - Classroom
-              </Typography> */}
-              {/* <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}
-              > */}
                 <Box>
                   <Typography className='pb-2 text-3xl font-semibold'>
                     Operating Systems - Classroom
@@ -35,15 +25,10 @@ export default function Page() {
               </div>
 
               <div className='float-right ml-auto'>
-               
-              {/* </div> */}
             </div>
            
             <Divider className='bg-slate-200' /> 
             <div style={{display:"flex",padding:"20px"}}>
-              {/* <Typography className='py-2 text-xl font-medium'>
-                Join Code : A3C1YT
-              </Typography>  */}
               <BorderedButton
                   onClick={() => {
                     console.log('Hello');
@@ -56,46 +41,12 @@ export default function Page() {
                     Thread: Solutions of 1st CT
                 </Typography>
             </div>
-            {/* <div className='my-2 flex gap-1'>
-              <Avatar size='sm'>JG</Avatar>
-              <Avatar size='sm'>RM</Avatar>
-              <Avatar size='sm'>SB</Avatar>
-              <Avatar size='sm'>+9</Avatar>
-
-              <Chip
-                className='float-right ml-auto'
-                color='success'
-                onClick={function () {}}
-                variant='outlined'
-              >
-                + Invite
-              </Chip>
-            </div> */}
             <div className='my-10'>
               <Thread />
             </div>
           </div>
-          {/* <div className='col-span-2 h-screen bg-light-gray p-3'>
-            <Typography className='p-2 text-xl font-medium'>Next Exams</Typography>
-            <ExamCard />
-            <ExamCard />
-            <ExamCard />
-          </div> */}
         </div>
       </section>
-      {/* <CardContent sx={{width:"full",background:"var(--clr-ghostwhite-100)"}}>
-      
-        <Card>
-            <CardContent>
-                <DiscussionCard/>
-                <DiscussionCard/>
-                <DiscussionCard/>
-                <DiscussionCard/>
-            </CardContent>
-
-        </Card>
-
-        </CardContent> */}
     </>
   );
 }
